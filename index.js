d89980f7bb2ecfc66a42d768560fc20f857e025e
rbahuchet <bahuchet@hpe.com>
2018-05-01 21:43:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
- var server = http.createServer(function(request, response) {
- 
-     response.writeHead(200, {"Content-Type": "text/plain"});
--    response.end("Hello Azure!");
-+    response.end("Hello World!");
- 
- });
- 
- var port = process.env.PORT || 1337;
- server.listen(port);
- 
--console.log("Server running at http://localhost:%d", port);
-+console.log("Server running at http://localhost:%d", port);
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
